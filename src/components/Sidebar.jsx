230ae8c0c0f9a8c991de2b09fb45678712be3050
Koding Kidd <kiddkoding@gmail.com>
2024-09-10 19:20:01 -0700

    Changed side color from black to blue

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Sidebar = () => (
   <nav className="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
-    <img src="professional.jpg" style={{ width: '100%' }} alt="Avatar" />
     <a href="#about" className="w3-bar-item w3-button w3-padding-large w3-hover-black">
       <i className="fa fa-user w3-xxlarge"></i>
       <p>ABOUT</p>
